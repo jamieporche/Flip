612166829fc1f8b891dc36a9043fcbb36e3a3e22
Jamie Porche <jamiemporche@gmail.com>
2022-01-31 14:46:55 -0500

    Added tables to schema.sql

--- java/database/schema.sql
+++ java/database/schema.sql
@@ -21,5 +21,44 @@ CREATE TABLE users (
 INSERT INTO users (username,password_hash,role) VALUES ('user','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','ROLE_USER');
 INSERT INTO users (username,password_hash,role) VALUES ('admin','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','ROLE_ADMIN');
 
+CREATE TABLE cards (
+	card_id serial NOT NULL,
+	front varchar(200) NOT NULL,
+	back varchar(500) NOT NULL,
+	user_id int NOT NULL,
+	CONSTRAINT PK_cards PRIMARY KEY (card_id),
+	CONSTRAINT FK_cards_users FOREIGN KEY (user_id) REFERENCES users(user_id)
+);
+
+CREATE TABLE decks (
+	deck_id serial NOT NULL,
+	deck_name varchar(100) NOT NULL,
+	user_id int NOT NULL,
+	isPublic boolean NOT NULL DEFAULT FALSE,
+	CONSTRAINT PK_decks PRIMARY KEY (deck_id),
+	CONSTRAINT FK_decks_users FOREIGN KEY (user_id) REFERENCES users(user_id)
+);
+
+CREATE TABLE card_deck (
+	card_id int NOT NULL,
+	deck_id int NOT NULL,
+	CONSTRAINT PK_card_deck PRIMARY KEY(card_id, deck_id),
+	CONSTRAINT FK_card_deck_card FOREIGN KEY (card_id) REFERENCES cards(card_id),
+	CONSTRAINT FK_card_deck_deck FOREIGN KEY (deck_id) REFERENCES decks(deck_id)
+);
+
+CREATE TABLE tags (
+	tag_id serial NOT NULL,
+	name varchar(100) NOT NULL,
+	CONSTRAINT PK_tags PRIMARY KEY (tag_id)
+);
+
+CREATE TABLE card_tag (
+	card_id int NOT NULL,
+	tag_id int NOT NULL,
+	CONSTRAINT PK_card_tag PRIMARY KEY(card_id, tag_id),
+	CONSTRAINT FK_card_tag_card PRIMARY KEY (card_id) REFERENCES cards(card_id),
+	CONSTRAINT FK_card_tag_tag PRIMARY KEY (tag_id) REFERENCES tags(tag_id)
+);
 
 COMMIT TRANSACTION;
