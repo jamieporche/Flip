76f84b4c74548bd427b8760bf29ebd18179aeebe
Sean Oberc <seanoberc@gmail.com>
2022-01-31 17:43:53 -0500

    JdbcDeck started

---
+++ java/src/main/java/com/techelevator/model/Card.java
@@ -0,0 +1,11 @@
+package com.techelevator.model;
+
+import java.util.UUID;
+
+public class Cards {
+    private int cardId;
+    private String frontOfCard;
+    private String backOfCard;
+    private int userId;
+
+}
---
+++ java/src/main/java/com/techelevator/model/Deck.java
@@ -0,0 +1,9 @@
+package com.techelevator.model;
+
+public class Decks {
+    private int deckId;
+    private String deckName;
+    private int userId;
+    private boolean isPublic;
+
+}
