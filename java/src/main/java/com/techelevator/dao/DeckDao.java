81ba6f3ff305b35f2680bf9843e9efcfa6371449
Dan Schnelzer <dlschnelzer27@gmail.com>
2022-02-08 09:08:16 -0500

    Submit and Public deck methods, issubmitted row added in DB

--- java/src/main/java/com/techelevator/dao/DeckDao.java
+++ java/src/main/java/com/techelevator/dao/DeckDao.java
@@ -10,5 +10,8 @@ public interface DeckDao {
     Deck createNewDeck(Deck deck);           // 3D. this will create a new deck
     List<Deck> getPublicDecks();        // 4D. this will get public decks
     void deleteDeck(int deckId);         // 5D. this will delete a deck
-    public Deck updateDeck(Deck deck);      // 6D. this will update an existing deck
+    Deck updateDeck(Deck deck);      // 6D. this will update an existing deck
+    Deck submitDeck(Deck deck);      // 7D. this will set a deck to be submitted
+    List<Deck> getSubmittedDecks();      // 8D. this will return a list of submitted decks
+    Deck makeDeckPublic(Deck deck);      // 9D. this will make Decks public
 }
